1c2186e2cfabe845a4a6ff4420cf1b4f5c08c9aa
Esperanza <esperanzasj2012@gmail.com>
2024-01-19 11:18:03 -0400

    Añadiendo ´section´ y ´especialidad´ a la base de datos

--- useradmin/add_user.php
+++ useradmin/add_user.php
@@ -3,9 +3,11 @@
 		$username = $_POST['username']; 
 		$password = $_POST['password']; 
 		$firstname = $_POST['firstname'];
-		$lastname = $_POST['lastname']; 
+		$lastname = $_POST['lastname'];
+		$section = $_POST['section']; 
+		$especialidad = $_POST['especialidad'];  
 		$idmedico = $_POST['idmedico']; 
-		$cimedico = $_POST['cimedico']; 
+		$cimedico = $_POST['cimedico'];
 		$conn = new mysqli("localhost", "root", "", "hcpms");
 		$q1 = $conn->query("SELECT * FROM `user` WHERE `username` = '$username'") or die(mysqli_error());
 		$f1 = $q1->fetch_array();
@@ -13,8 +15,8 @@
 			if($c1 > 0){
 				echo "<script>alert('Username already taken')</script>";
 			}else{
-				$conn->query("INSERT INTO `user` VALUES('', '$username', '$password', '$firstname', '$lastname', '$idmedico', '$cimedico')");
+				$conn->query("INSERT INTO `user` VALUES('', '$username', '$password', '$firstname', '$lastname', '$section', '$especialidad', '$idmedico', '$cimedico')");
 				header("location: user.php");
 			}
 	}
-		
+?>
